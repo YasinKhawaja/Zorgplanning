c3c7d755d645bddef3ece2e59dce22e686aab828
Yasin Khawaja <yasin.khawaja@ventigrate.be>
2022-06-15 20:49:07 +0200

    Logo Font Fix

--- CP.React/ClientApp/src/components/presentations/Header.jsx
+++ CP.React/ClientApp/src/components/presentations/Header.jsx
@@ -134,7 +134,6 @@ function Logo() {
         sx={{
           mr: 2,
           display: { xs: "none", md: "flex" },
-          fontFamily: "monospace",
           fontWeight: 700,
           letterSpacing: ".3rem",
           color: "inherit",
@@ -170,7 +169,6 @@ function LogoResponsive() {
           mr: 2,
           display: { xs: "flex", md: "none" },
           flexGrow: 1,
-          fontFamily: "monospace",
           fontWeight: 700,
           letterSpacing: ".3rem",
           color: "inherit",
